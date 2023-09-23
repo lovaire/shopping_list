ffaa2261ef89d29d22f58f47ceeb0f4dab0cc9fc
lovaire <rifsrahmt@gmail.com>
2023-09-23 20:29:54 +0700

    commitkuw

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from main.views import show_main, create_product, show_xml, show_json, show_xml_by_id, show_json_by_id 
+from main.views import show_main, create_product, show_xml, show_json, show_xml_by_id, show_json_by_id, delete
 from main.views import register #sesuaikan dengan nama fungsi yang dibuat
 from main.views import login_user
 from main.views import logout_user
@@ -16,4 +16,5 @@ urlpatterns = [
     path('register/', register, name='register'),
     path('login/', login_user, name='login'),
     path('logout/', logout_user, name='logout'),
+    path('delete/<int:id>/', delete, name='delete'), 
 ]
--- main/views.py
+++ main/views.py
@@ -37,6 +37,9 @@ def create_product(request):
      product.save()
      return HttpResponseRedirect(reverse('main:show_main'))
 
+ context = {'form': form}
+ return render(request, "create_product.html", context)
+
 def show_xml(request):
     data = Product.objects.all()
     return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
@@ -52,6 +55,12 @@ def show_xml_by_id(request, id):
 def show_json_by_id(request, id):
     data = Product.objects.filter(pk=id)
     return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+def delete(request, id):
+    data = Product.objects.get(pk=id)
+    data.delete()
+    return HttpResponseRedirect(reverse('main:show_main'))
+
 
 def register(request):
     form = UserCreationForm()
